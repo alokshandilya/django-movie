6028dfca74453acf50c4cadc3cb7aeb261c1bbcf
alokshandilya <alok.shandilya10@gmail.com>
2024-10-12 00:41:38 +0530

    refactor: Add base template for movies app
    This commit adds a base template for the movies app. The base template
    contains a navigation bar with links to the home and about pages. The
    home and about pages extend the base template and display the content in
    the content block.

--- movies/urls.py
+++ movies/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from .views import index, about
 
 urlpatterns = [
-    path("", index),
-    path("about/", about),
+    path("", index, name="home"),
+    path("about/", about, name="about"),
 ]
