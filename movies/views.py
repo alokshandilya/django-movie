cb66345cdcdc20bd60ad79949db29b0e4501e90b
alokshandilya <alok.shandilya10@gmail.com>
2024-10-11 23:22:40 +0530

    feat: add movies list to index page
    use django context to pass movies list to index page and django template
    to render the list of movies using "title" filter to capitalize the
    first letter of each word in the movie name.

--- movies/views.py
+++ movies/views.py
@@ -3,7 +3,17 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, "movies/index.html")
+    return render(
+        request,
+        "movies/index.html",
+        context={
+            "movies": [
+                "the matrix",
+                "the matrix reloaded",
+                "the matrix revolutions",
+            ],
+        },
+    )
 
 
 def about(request):
