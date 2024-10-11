d048344cd1f7804fc6aa076d57634fbd11ab9c1c
alokshandilya <alok.shandilya10@gmail.com>
2024-10-11 10:27:26 +0530

    feat: Add about page
    - add about.html in movies/templates/movies/about.html
    - add about view in movies/views.py
    - add about path in movies/urls.py

--- movies/views.py
+++ movies/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, "movies/index.html")
+
+
+def about(request):
+    return render(request, "movies/about.html")
